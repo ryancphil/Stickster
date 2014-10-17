28cfe0e8fff4c552bd3ba66c2dea6b439ec941de
Matt <barr.matt11@gmail.com>
2014-10-17 17:58:42 -0400

    Camera takes pictures and saves to imageview

--- app/src/main/java/com/mjr/ryan/stickster/MainActivity.java
+++ app/src/main/java/com/mjr/ryan/stickster/MainActivity.java
@@ -2,17 +2,21 @@ package com.mjr.ryan.stickster;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
-
+    ImageView imgFavorite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgFavorite = (ImageView)findViewById(R.id.imageView1);
 
         CameraFragment cameraFragment = new CameraFragment();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
@@ -23,7 +27,20 @@ public class MainActivity extends Activity {
         // Commit the transaction
         fragmentTransaction.commit();
 
+        open();
+
     }
 
+    public void open(){
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgFavorite.setImageBitmap(bp);
+    }
 }
